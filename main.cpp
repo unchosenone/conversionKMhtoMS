bb23f236ca5c6a8d00ba3137a618f7b1ae55e3c7
Gean Michel S <39742685+unchosenone@users.noreply.github.com>
2018-05-29 17:11:54 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+ float km, ms ;
+
+
+ int main(){
+ 
+ printf("conversão de km/h para m/s\n");	
+  printf("digite a velocidade em Km/h\n");
+  scanf("%f", &km);
+ 
+ 
+  ms = km / 3.6;
+ 
+  printf("a velocidade em M/S e %f\n", ms);
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+}
+
